2c3f310ce437e17d2cf3780a4869afd69e388f1b
cxmhfut <912750125@qq.com>
2018-11-22 18:31:06 +0800

    Controller简介及Action定义

--- src/main/java/com/cxmhfut/controller/IndexController.java
+++ src/main/java/com/cxmhfut/controller/IndexController.java
@@ -3,8 +3,13 @@ package com.cxmhfut.controller;
 import com.jfinal.core.Controller;
 
 public class IndexController extends Controller {
+    /**
+     * action定义模板
+     */
     public void index() {
-        setAttr("msg","Hello JFinal 3.0");
+        String msg = getPara("msg", "defaultMessage");
+        String values[] = getParaValues("test");//checkbox
+        setAttr("msg", "Hello JFinal 3.0----->" + msg);
         renderTemplate("index.html");
     }
 }
